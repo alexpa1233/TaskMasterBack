eadc02243b81dd270a5603af657b771f13426d0d
alexpa1233 <alexpa12333@gmail.com>
2023-12-07 06:19:28 +0100

    feat: :bug:

--- src/main/java/com/Alejandro/TFG/model/Work.java
+++ src/main/java/com/Alejandro/TFG/model/Work.java
@@ -16,7 +16,7 @@ public class Work {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy = "Work")
+    @OneToMany(mappedBy = "work")
     private List<WorkCheckBox> workCheckBox;
 
     @OneToOne
